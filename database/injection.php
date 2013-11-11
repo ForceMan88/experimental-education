6ba248104c5976b46815bc12f937fc750186f1da
Arthur Galanzovskiy <a.galanzovskiy@ism-ukraine.com>
2013-11-11 15:49:39 +0200

    Commit.

--- database/injection.php
+++ database/injection.php
@@ -1,2 +1,30 @@
 <?php
 include_once('db.php');
+#!/bin/sh
+$mysqli = new mysqli('localhost', 'root', 'abcABC123', 'one_or_one_vulnerable');
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+echo 'Success... ' . $mysqli->host_info . "\n";
+
+if ($mysqli->query(
+        "CREATE TEMPORARY TABLE `test_data` (
+          `id` int(10) unsigned NOT NULL,
+          `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+          `score` int(10) unsigned NOT NULL,
+          PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;"
+    ) === true) {
+    printf("Table test_data successfully created.\n");
+}
+
+$mysqli->query("INSERT INTO `test_data` (`id`, `name`, `score`) VALUES (1, 'Slava', 1), (2, 'Serg', 3);");
+
+$input = mysql_real_escape_string($argv[1]);
+
+$res = $mysqli->query("SELECT * FROM `test_data` WHERE `score` = $input");
+while ($row = $res->fetch_assoc()) {
+    echo " id = " . $row['id'] . "\n";
+}
+
+$mysqli->close();
--- database/mysqlli.php
+++ database/mysqlli.php
@@ -1,1 +1,23 @@
 <?php
+include_once("db.php");
+
+$db = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+
+$statement_one = $db->prepare('SELECT `name` FROM `users` WHERE `username` = ?');
+$name = 'Bob';
+$statement_one->bind_param('s', $name);
+$statement_one->execute();
+
+$statement_two = $db->prepare('SELECT * FROM `users` WHERE `username` = ? AND `$height` = ? AND `age` = ?' );
+$height  = 185.5;
+$age  = 12;
+//string, integer, blob or double
+$statement_two->bind_param('sdi', $name, $height, $age);
+$statement_two->execute();
+
+
+
