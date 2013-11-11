c44632b2b88fdd1a92ffb8917eb9a45ef67a4df6
Arthur Galanzovskiy <a.galanzovskiy@ism-ukraine.com>
2013-11-11 18:17:34 +0200

    Linked list, spl

---
+++ programming structure/spl/view.php
@@ -0,0 +1,24 @@
+<table width="100%" style="td:first{color:red}">
+    <colgroup>
+        <?php foreach ($obj as $key => $value) : ?>
+            <col width="<?php echo 100/$obj->count() . '%' ?>"/>
+        <?php endforeach ?>
+        <col width="100%"/>
+    </colgroup>
+    <tr>
+        <?php foreach ($obj as $key => $value) : ?>
+
+            <td bgcolor="<?php echo ($obj->getCurrent() == $value) ? "red" : "#deb887" ?>" height="60px" align="center"><?php echo $value ?></td>
+
+        <?php endforeach ?>
+    </tr>
+</table>
+</br></br></br>
+<form method="post">
+    <input type="submit" name="move_left" value="Move left">
+    <input type="submit" name="move_right" value="Move right"> </br></br>
+    <input type="submit" name="fifo_add" value="FIFO add">
+    <input type="submit" name="fifo_delete" value="FIFO delete"></br></br>
+    <input type="submit" name="lifo_add" value="LIFO add">
+    <input type="submit" name="lifo_delete" value="LIFO delete">
+</form>
