a005634617a3f12c99541241585941f04a409cd6
Arthur Galanzovskiy <a.galanzovskiy@ism-ukraine.com>
2013-12-02 17:57:50 +0200

    quicksort

--- sort/quickstor.php
+++ sort/quickstor.php
@@ -7,44 +7,37 @@ $array = [81, 1, 21, 91, 51, 41, 61, 71, 31, 11];
 
 echo "Start array = " . implode("  ", $array) . "</br>";
 
-
-function mysort(&$array, $start = 0, $end = 0)
+function mysort($array)
 {
-    $length = count($array);
-//    $wickPoint = (int) (count($array)/2);
+//    $wickPointKey = (int)(count($array) / 2);
+    $wickPoint = $array[0];
+    $newArray = array();
+    for ($start = 0; $start < count($array); $start++) {
 
-    for ($start; $start <= count($array)/2; $start++) {
-        $end = $length - $start - 1;
-
-        if ($array[$start] > $array[$wickPoint] && $array[$end] < $array[$wickPoint]) {
-            $var = $array[$end];
-            $array[$end] = $array[$start];
-            $array[$start] = $var;
-            continue;
+        if ($array[$start] > $wickPoint) {
+            array_push($newArray, $array[$start]);
+        } else {
+            array_unshift($newArray, $array[$start]);
         }
-
-//        if ($array[$start] > $array[$wickPoint]) {
-//            $array[] = $array[$start];
-//            array_splice($array, $start, 1);
-//        }
-//
-//        if ($array[$end] < $array[$wickPoint]) {
-//            $temp = $array[$end];
-//            array_splice($array, $end, 1);
-//            array_unshift($array, $temp );
-//        }
     }
+
+    if (count($newArray) >= 2) {
+           $smallOne = (count(array_slice($newArray, 0, (int)(count($newArray) / 2))) >= 2) ? mysort(array_slice($newArray, 0, (int)(count($newArray) / 2))) : array();
+           $smallTwo = (count(array_slice($newArray, (int)(count($newArray) / 2))) >= 2) ? mysort(array_slice($newArray, (int)(count($newArray) / 2))) : array();
+          $newArray = array_merge($smallOne, $smallTwo);
+
+
+    }
+
+    return $newArray;
+
 }
 
 function quickSort(&$array)
 {
-    mysort($array, (int) count($array) / 2);
-    var_dump($array);
-    mysort($array, (int) count($array) / 2);
-//    mysort($array, (int) count($array)/2);
+    return mysort($array);
 }
 
-quickSort($array);
 //mysort($array, 5);
 
-echo "Result array = " . implode("  ", $array);
+echo "Result array = " . implode("  ", quickSort($array));
