a848d9bf233a31f9f4e2bb3542d40dc345600477
Arthur Galanzovskiy <forceman88@gmail.com>
2013-12-16 21:35:38 +0200

    merge, selection sort,quicksort

---
+++ sort/comparing.php
@@ -0,0 +1,62 @@
+<style type="text/css">
+    .tg-table-paper { border-collapse: collapse; border-spacing: 0; }
+    .tg-table-paper td, .tg-table-paper th { background-color: #F3F5EF; border: 1px #bbb solid; color: #333; font-family: sans-serif; font-size: 100%; padding: 10px; vertical-align: top; }
+    .tg-table-paper .tg-even td  { background-color: #F0F0E5; }
+    .tg-table-paper th  { background-color: #EAE2CF; color: #333; font-size: 110%; font-weight: bold; }
+    .tg-table-paper tr:hover td, .tg-table-paper tr.even:hover td  { color: #222; background-color: #FFFBEF; }
+    .tg-bf { font-weight: bold; } .tg-it { font-style: italic; }
+    .tg-left { text-align: left; } .tg-right { text-align: right; } .tg-center { text-align: center; }
+</style>
+<table class="tg-table-paper">
+    <tr>
+        <th></th>
+        <th colspan="2">Quick</th>
+        <th colspan="2">Merge</th>
+        <th colspan="2">Insertion</th>
+        <th colspan="2">Selection</th>
+    </tr>`
+    <tr class="tg-even">
+        <td></td>
+        <td>Time()</td>
+        <td>Memory()</td>
+        <td>Time()</td>
+        <td>Memory()</td>
+        <td>Time()</td>
+        <td>Memory()</td>
+        <td>Time()</td>
+        <td>Memory()</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr class="tg-even">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
